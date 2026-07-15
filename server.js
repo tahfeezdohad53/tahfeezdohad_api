ff541039f752e586246ac8bd363d714ef7e28545
unknown <huzefaratlam63@gmail.com>
2026-07-15 12:42:25 +0530

    commented server.js file

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app.js";
+// import mongoose from "mongoose";
+// import app from "./app.js";
 
 
-(async function(){
-    try{
-        const r = await mongoose.connect(process.env.MONGO_URI);
-        console.log('connected');
+// (async function(){
+//     try{
+//         const r = await mongoose.connect(process.env.MONGO_URI);
+//         console.log('connected');
 
-    }catch(err){
-        console.log(err);
-    } 
-})()
+//     }catch(err){
+//         console.log(err);
+//     } 
+// })()
 
-app.listen(process.env.PORT,() => {
-    console.log('listening');
-})
+// app.listen(process.env.PORT,() => {
+//     console.log('listening');
+// })
