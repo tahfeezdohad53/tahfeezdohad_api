6697eb6954ea751b37c5c77b76524e8b8b956341
unknown <huzefaratlam63@gmail.com>
2026-07-25 11:32:44 +0530

    allowing teacher to change multiple diary and assign proxy

--- controller/student.js
+++ controller/student.js
@@ -22,14 +22,14 @@ export const handleChangeDiary = catchAsync(async (req,res,next) => {
 export const handleChangeMultipleDiary = catchAsync(async (req,res,next) => {
     const {teacherId,studentsId} = req.body;
     const {id,role} = req.user;
-    if(role !== 'admin') return res.status(400).json({ok:false,message:'you are not allowed for this action'});
+    // if(role !== 'admin') return res.status(400).json({ok:false,message:'you are not allowed for this action'});
     await User.updateMany({role:'student',_id:{$in:studentsId}},{teacher:teacherId});
     res.status(200).json({ok:true});
 })
 export const handleAssignMultipleProxies = catchAsync(async (req,res,next) => {
     const {teacherId,studentsId} = req.body;
     const {id,role} = req.user;
-    if(role !== 'admin') return res.status(400).json({ok:false,message:'you are not allowed for this action'});
+    // if(role !== 'admin') return res.status(400).json({ok:false,message:'you are not allowed for this action'});
     await User.updateMany({role:'student',_id:{$in:studentsId}},{proxyTeacher:teacherId});
     res.status(200).json({ok:true});
 })
