fcab38f3779380a0bf86d94c77ae73c34a1fa878
unknown <huzefaratlam63@gmail.com>
2026-08-04 23:59:40 +0530

    forgot to add resend api key

--- libs/resend.js
+++ libs/resend.js
@@ -3,6 +3,6 @@ import { Resend } from "resend";
 
 configDotenv();
 
-const resend = new Resend(process.env.API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default resend;
