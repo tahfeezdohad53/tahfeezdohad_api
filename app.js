3437e1d9ccb85c37d8f878a12961c4ac253501f7
unknown <huzefaratlam63@gmail.com>
2026-08-15 20:25:51 +0530

    even callback not working in cors so now passing two env var as array

--- app.js
+++ app.js
@@ -27,16 +27,9 @@ import nodeCron from "node-cron";
 configDotenv();
 const app = express();
 const server = http.createServer(app);
-const allowedOrigins = process.env.URL.split(",");
 const io = new Server(server, {
   cors: {
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [process.env.URL,process.env.URL2],
     methods: ["GET", "POST"],
     credentials: true,
   },
