80ba3abfa4cafef5eff47decc63e27d7e3b64d8a
unknown <huzefaratlam63@gmail.com>
2026-08-15 20:29:53 +0530

    now added url2 first

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [process.env.URL,process.env.URL2],
+    origin: [process.env.URL2, process.env.URL],
     methods: ["GET", "POST"],
     credentials: true,
   },
